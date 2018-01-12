0a94cc31485a64c29fcd409cdc99100c59487f4d
nzuchna <n.zuchna@gmail.com>
2018-01-12 11:43:07 +0100

    Added Sign-In Design + Resources

--- Android-Development/app/src/main/java/com/identos/smarthomecare_app/MainActivity.java
+++ Android-Development/app/src/main/java/com/identos/smarthomecare_app/MainActivity.java
@@ -6,7 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     public static final String EXTRA_MESSAGE = "com.identos.smarthomecare_app";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.input_email) edtEmail _emailText;
+    @InjectView(R.id.input_password) EditText _passwordText;
+    @InjectView(R.id.btn_login) Button _loginButton;
+    @InjectView(R.id.link_signup) TextView _signupLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
     public void scanBLEDevices(View view){
             Intent intent = new Intent(this, MonitoringActivity.class);
             startActivity(intent);
+    }
+
+    public void Login(View view){
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
